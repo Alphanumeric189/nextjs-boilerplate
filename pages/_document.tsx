4c4876511064b0a139adf11a73f0ffae3b5f95a8
Alphanumeric189 <Alphanumeric189@gmail.com>
2025-06-26 23:54:15 +0100

    Create _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/* AdSense Meta */}
+        <meta name="google-adsense-account" content="ca-pub-3339448910741341" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
